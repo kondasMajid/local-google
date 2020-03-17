00e0ad7cbbe99d752bb7f0764b22cfbcba05390c
kondasMajid <kondasmajid@gmail.com>
2020-03-17 10:40:43 -0800

    UI arrangment

--- src/app/index/index.component.ts
+++ src/app/index/index.component.ts
@@ -1,5 +1,6 @@
 import { GoogleService } from './../shared/google.service';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-index',
@@ -8,11 +9,79 @@ import { Component, OnInit } from '@angular/core';
 })
 export class IndexComponent implements OnInit {
 
-  constructor(private google: GoogleService) { }
+ 
+  
+  title = 'google';
+  googleData : any[];
+  
+  searchword: string="";
+  passDataSeconds: any[]=[];
+  
+  passDataResults: any[]=[];
 
-  ngOnInit() {
-    // this.google.getGoogle().s
-    
+  inputKeyword :  string = '';
+  allGoogleData;
+  nextpage;
+  
+
+  convert;
+  // inputKeyword: FormControl;
+  // this.inputKeyword =
+  constructor(private googleService: GoogleService, private route:Router){
+    // this.inputKeyword = new FormControl();
+  //  this.inputKeyword =  this.googleService.input;
   }
+
+
+  @Input() search: string; 
+  
+  ngOnInit() {  
+  //  this.inputKeyword = this.googleService.input
+  // this.searchData();
+    
+  } 
+
+  nextPage(){
+    return this.googleService.getGoogle(this.inputKeyword)
+      .subscribe(y => {
+        this.nextpage = y;
+        this.route.navigate(['/nextpage'])
+        console.log(this.nextpage)
+      });
+    
+    // console.log('hello next page')
+  }
+
+  searchData() { 
+      return this.googleService.getGoogle(this.inputKeyword).subscribe(x =>{
+       
+        
+         //returning the titles,ddescription,link, displaylinks etc
+       this.googleData = x.items;
+       console.log('googleData', this.googleData);
+       
+       //returung the number of results found by the search
+       this.passDataResults = x.searchInformation.totalResults;
+      //  console.log('passDataResults', this.passDataResults);
+        
+        //returning the result of the seconds to find result
+       this.passDataSeconds = x.searchInformation.formattedSearchTime;
+      //  console.log('passDataSeconds', this.passDataSeconds);
+        
+
+       this.convert = x.queries;
+      //  console.log('convert', this.convert);
+
+    
+        
+       
+      //  this.googleData= x;
+        // console.log('googleData',this.googleData)
+       }, error => {
+         console.log("error here",error)
+       })
+       
+ }
+
 
 } 
